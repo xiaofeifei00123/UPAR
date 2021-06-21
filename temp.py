a7daea71de39aeb28551adc187dff08935954a27
forxd <2951985709@qq.com>
2021-06-21 16:53:59 +0800

    获取wrfout数据中所有需要的值, 包括变量和它们的梯度，缺少对于站点数据的处理

--- temp.py
+++ temp.py
@@ -27,33 +27,59 @@ import netCDF4 as nc
 from netCDF4 import Dataset
 from wrf import getvar, vinterp, interplevel
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MultipleLocator,FormatStrFormatter
-import cmaps 
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+import cmaps
 from get_cmap import get_cmap_temp
+
 
 class Upar():
     pass
+
     def __init__(var):
         pass
+
 
 class Get_data():
     """
     读取，处理数据
     """
-    def __init__(self,):
-        pass    
+    def __init__(self, ):
+        pass
 
-    def get_data_single(self, station={'lat':32.4, 'lon':80.1, 'name':'ShiQuanhe', 'number':'55228'}):
+    def get_data_single(self,
+                        station={
+                            'lat': 32.4,
+                            'lon': 80.1,
+                            'name': 'ShiQuanhe',
+                            'number': '55228'
+                        }):
         """读取单个变量,5次wrf试验的数据
         """
         # station={'lat':32.3, 'lon':84.0, 'name':'GaiZe'}
         #### 循环出需要进行处理的单个试验
-        pressure_level = [600, 575, 550, 525, 500, 450, 400, 350, 300, 250, 200, 150, 100]
+        pressure_level = [
+            600, 575, 550, 525, 500, 450, 400, 350, 300, 250, 200, 150, 100
+        ]
         ## 站点
-        station_dic={
-            'GaiZe':{'lat':32.3, 'lon':84.0, 'name':'GaiZe','number':'55248'},
-            'ShenZha':{'lat':30.9, 'lon':88.7, 'name':'ShenZha', 'number':'55472'},
-            'ShiQuanhe':{'lat':32.4, 'lon':80.1, 'name':'ShiQuanhe', 'number':'55228'},
+        station_dic = {
+            'GaiZe': {
+                'lat': 32.3,
+                'lon': 84.0,
+                'name': 'GaiZe',
+                'number': '55248'
+            },
+            'ShenZha': {
+                'lat': 30.9,
+                'lon': 88.7,
+                'name': 'ShenZha',
+                'number': '55472'
+            },
+            'ShiQuanhe': {
+                'lat': 32.4,
+                'lon': 80.1,
+                'name': 'ShiQuanhe',
+                'number': '55228'
+            },
         }
         var = 'temp'  # 先就处理温度, 后面的数据是需要处理得到的
         month = 'Jul'  # 后面进行循环即可
@@ -66,7 +92,8 @@ class Get_data():
         model_dic = {}
         for model in model_list:
             # flnm_temp = os.path.join(path, 'temp_Jul_YSU_latlon')
-            file_name = str(var)+"_"+str(month)+"_"+str(model)+"_latlon"
+            file_name = str(var) + "_" + str(month) + "_" + str(
+                model) + "_latlon"
             flnm_var = os.path.join(path, file_name)
 
             ds_var = xr.open_dataset(flnm_var)
@@ -80,7 +107,9 @@ class Get_data():
     def get_data_single_once(self, var, flnm_var, station):
         '''单个变量，单个试验的数据读取'''
         pass
-        pressure_level = [600, 575, 550, 525, 500, 450, 400, 350, 300, 250, 200, 150, 100]
+        pressure_level = [
+            600, 575, 550, 525, 500, 450, 400, 350, 300, 250, 200, 150, 100
+        ]
 
         ds_var = xr.open_dataset(flnm_var)
         da_var = ds_var[var]
@@ -91,9 +120,8 @@ class Get_data():
         # model_dic[model] = da_var
         # return model_dic
 
-
     def get_data_t_td(self, station):
-        pass    
+        pass
         model_list = ['ACM2', 'YSU', 'QNSE', 'QNSE_EDMF', 'TEMF']
         var1 = 'temp'  # 先就处理温度, 后面的数据是需要处理得到的
         var2 = 'td'  # 先就处理温度, 后面的数据是需要处理得到的
@@ -102,18 +130,20 @@ class Get_data():
         for model in model_list:
             pass
             path = '/mnt/zfm_18T/Asses_PBL/wrfout_data/'
-            file_name1 = str(var1)+"_"+str(month)+"_"+str(model)+"_latlon"
+            file_name1 = str(var1) + "_" + str(month) + "_" + str(
+                model) + "_latlon"
             flnm_var1 = os.path.join(path, file_name1)
-            file_name2 = str(var2)+"_"+str(month)+"_"+str(model)+"_latlon"
+            file_name2 = str(var2) + "_" + str(month) + "_" + str(
+                model) + "_latlon"
             flnm_var2 = os.path.join(path, file_name2)
             temp = self.get_data_single_once(var1, flnm_var1, station)
             td = self.get_data_single_once(var2, flnm_var2, station)
-            model_dic[model] = temp-td
+            model_dic[model] = temp - td
         return model_dic
 
 
     def get_data_wind(self, station):
-        pass    
+        pass
         model_list = ['ACM2', 'YSU', 'QNSE', 'QNSE_EDMF', 'TEMF']
         var1 = 'U'  # 先就处理温度, 后面的数据是需要处理得到的
         var2 = 'V'  # 先就处理温度, 后面的数据是需要处理得到的
@@ -122,17 +152,19 @@ class Get_data():
         for model in model_list:
             pass
             path = '/mnt/zfm_18T/Asses_PBL/wrfout_data/'
-            file_name1 = str(var1)+"_"+str(month)+"_"+str(model)+"_latlon"
+            file_name1 = str(var1) + "_" + str(month) + "_" + str(
+                model) + "_latlon"
             flnm_var1 = os.path.join(path, file_name1)
-            file_name2 = str(var2)+"_"+str(month)+"_"+str(model)+"_latlon"
+            file_name2 = str(var2) + "_" + str(month) + "_" + str(
+                model) + "_latlon"
             flnm_var2 = os.path.join(path, file_name2)
             U = self.get_data_single_once(var1, flnm_var1, station)
             V = self.get_data_single_once(var2, flnm_var2, station)
-            model_dic[model] = xr.ufuncs.sqrt(U**2+V**2)
+            model_dic[model] = xr.ufuncs.sqrt(U**2 + V**2)
         return model_dic
 
     def get_data_temp(self, station):
-        pass    
+        pass
         model_list = ['ACM2', 'YSU', 'QNSE', 'QNSE_EDMF', 'TEMF']
         var = 'temp'  # 先就处理温度, 后面的数据是需要处理得到的
         # var2 = 'V'  # 先就处理温度, 后面的数据是需要处理得到的
@@ -141,11 +173,43 @@ class Get_data():
         for model in model_list:
             pass
             path = '/mnt/zfm_18T/Asses_PBL/wrfout_data/'
-            file_name = str(var)+"_"+str(month)+"_"+str(model)+"_latlon"
+            file_name = str(var) + "_" + str(month) + "_" + str(
+                model) + "_latlon"
             flnm_var = os.path.join(path, file_name)
             temp = self.get_data_single_once(var, flnm_var, station)
             model_dic[model] = temp
         return model_dic
+
+    def grads_data(self, model_dic):
+        """根据输入的垂直文件，求它做梯度后的文件
+
+        Args:
+            model_dic ([type]): [description]
+        """
+        pass
+        # print("yes")
+        model_list = ['ACM2', 'YSU', 'QNSE', 'QNSE_EDMF', 'TEMF']
+        dic_return = {}
+        for model in model_list:
+            ser = []
+            da = model_dic[model]
+            pressure = da.pressure.values
+            ## 第0层是600hPa
+            for i in range(len(pressure)):
+                if i == 0:
+                    sr = (da[:,i+1]-da[:,i])/(pressure[i+1]-pressure[i])  # 边界上使用前差或后差
+                    ser.append(sr)
+                elif i>0 and i<len(pressure)-1:
+                    sr = (da[:,i+1]-da[:,i-1])/(pressure[i]-pressure[i-1])/2   # 中间的使用中央差分
+                    ser.append(sr)
+                elif i==len(pressure)-1:
+                    pass
+                    sr = (da[:,i]-da[:,i-1])/(pressure[i]-pressure[i-1])
+                    ser.append(sr)
+            da = xr.concat(ser, 'pressure')
+            da.coords['pressure'] = pressure
+            dic_return[model] = da
+        return dic_return
 
 
     def get_data_main(self, var, station):
@@ -161,7 +225,22 @@ class Get_data():
             model_dic = self.get_data_wind(station)
             print(model_dic)
             pass
-        elif var == 'grads':   ## 梯度
+        elif var == 'temp_grads':  ## 梯度
+            model_dic = self.get_data_temp(station)
+            model_dic = self.grads_data(model_dic)
+            print(model_dic)
+            pass
+
+        elif var == 't_td_grads':  ## 梯度
+            model_dic = self.get_data_t_td(station)
+            model_dic = self.grads_data(model_dic)
+            print(model_dic)
+            pass
+
+        elif var == 'wind_grads':  ## 梯度
+            model_dic = self.get_data_wind(station)
+            model_dic = self.grads_data(model_dic)
+            print(model_dic)
             pass
 
         return model_dic
@@ -171,21 +250,24 @@ class Regrid():
     """对数据进行插值, 
     水平插值和垂直插值
     """
-    def get_pressure_lev(self, ):
+    def get_pressure_lev(self, station):
         #### 得到各层的pressure值
-        ## 不同时刻各层气压值，差别可以忽略不计, 
+        ## 不同时刻各层气压值，差别可以忽略不计,
         # 后面还要对气压层进行插值, 这里不对它做过高精度要求
         path = '/mnt/zfm_18T/Asses_PBL/wrfout_data/'
         flnm_pressure = os.path.join(path, 'pressure_Jul_YSU_latlon')
         ds_pressure = xr.open_dataset(flnm_pressure)
         pr = ds_pressure.pressure
         prb = pr.sel(time='2016-07-01 13:00')
-        prc = prb.sel(lat=32.13, lon=92.5, method='nearest')
+        lat = station['lat']
+        lon = station['lon']
+        # prc = prb.sel(lat=32.13, lon=92.5, method='nearest')
+        prc = prb.sel(lat=lat, lon=lon, method='nearest')
         return prc
 
     def regrid(self, da_temp, station, pressure_lev):
-        """[summary]
-
+        """对wrfout数据进行插值的
+        需要水平插值和垂直插值两项
         Args:
             da_temp ([DataArray]): [需要插值的变量]
 
@@ -193,26 +275,28 @@ class Regrid():
             [type]: [description]
         """
 
-        #### 将bottom_top坐标换成气压坐标 
+        #### 将bottom_top坐标换成气压坐标
         time_coord = da_temp.time.values
         lat_coord = da_temp.lat.values
         lon_coord = da_temp.lon.values
-
-        prc = self.get_pressure_lev()
+        # loc = {'lat':station['lat'], 'lon'}
+        prc = self.get_pressure_lev(station)
         pressure_coord = prc.values
         # print(pressure_coord)
 
         da_temp_reset = da_temp.values
         # print(da_temp_reset)
-        da_temp = xr.DataArray(da_temp_reset, 
-                coords=[time_coord, pressure_coord, lat_coord, lon_coord],
-                dims=['time', 'pressure', 'lat', 'lon']
-                )
+        da_temp = xr.DataArray(
+            da_temp_reset,
+            coords=[time_coord, pressure_coord, lat_coord, lon_coord],
+            dims=['time', 'pressure', 'lat', 'lon'])
 
         #### 对换成气压坐标的temp进行插值
         ## 水平插值
         # da_temp = da_temp.sel(lat=33.5, lon=97.5, method='nearest')
-        da_temp = da_temp.sel(lat=station['lat'], lon=station['lon'], method='nearest')
+        da_temp = da_temp.sel(lat=station['lat'],
+                              lon=station['lon'],
+                              method='nearest')
         ## 垂直插值
         da_temp_return = da_temp.interp(pressure=pressure_lev)
         # print(da_temp_return)
@@ -220,35 +304,47 @@ class Regrid():
 
 
 class Draw():
-
-    def __init__(self,):
+    def __init__(self, ):
         # self.da_var = da_var
         # self.name = name
         # self.station = station
         pass
 
-    def draw_main(self,):
+    def draw_main(self, ):
 
-
-        station_dic={
-            'GaiZe':{'lat':32.3, 'lon':84.0, 'name':'GaiZe','number':'55248'},
-            'ShenZha':{'lat':30.9, 'lon':88.7, 'name':'ShenZha', 'number':'55472'},
-            'ShiQuanhe':{'lat':32.4, 'lon':80.1, 'name':'ShiQuanhe', 'number':'55228'},
+        station_dic = {
+            'GaiZe': {
+                'lat': 32.3,
+                'lon': 84.0,
+                'name': 'GaiZe',
+                'number': '55248'
+            },
+            'ShenZha': {
+                'lat': 30.9,
+                'lon': 88.7,
+                'name': 'ShenZha',
+                'number': '55472'
+            },
+            'ShiQuanhe': {
+                'lat': 32.4,
+                'lon': 80.1,
+                'name': 'ShiQuanhe',
+                'number': '55228'
+            },
         }
         # var = 'temp'
         # var = 't_td'
         var = 'wind'
         for key in station_dic:
-            pass 
-            station =  station_dic[key]
+            pass
+            station = station_dic[key]
 
-
-        ### 获得数据
+            ### 获得数据
             gd = Get_data()
             model_dic = gd.get_data_main(var, station)
-        # print("yes")
+            # print("yes")
 
-        ## 画图
+            ## 画图
             bb = self.combine_fig(var, model_dic, station['name'])
 
     def draw_contourf_single(self, var, ax, val, title, time_index):
@@ -271,7 +367,7 @@ class Draw():
         # time_index = time_index['data']
         val = val.sel(time=time_index)
 
-        val = val.values.swapaxes(0,1)  # 转置矩阵
+        val = val.values.swapaxes(0, 1)  # 转置矩阵
         color_map = get_cmap_temp()
 
         if var == 'temp':
@@ -281,14 +377,19 @@ class Draw():
         elif var == 'wind':
             level = np.arange(0, 15, 1)
 
-        CS = ax.contourf(x,y,val,levels=level, cmap=color_map, extend='both')
+        CS = ax.contourf(x,
+                         y,
+                         val,
+                         levels=level,
+                         cmap=color_map,
+                         extend='both')
         # cb = fig.colorbar(CS, orientation='horizontal', shrink=0.8, pad=0.14, fraction=0.14) # 这里的cs是画填色图返回的对象
         ## 设置标签大小
         ax.set_xticks(x[::2])  # 这个是选择哪几个坐标画上来的了,都有只是显不显示
         ax.set_xticklabels(x[::2].strftime('%m%d'))
 
         plt.gca().invert_yaxis()
-        ax.set_ylim(600,300)
+        ax.set_ylim(600, 300)
         ax.set_title(title)
         # ## 设置标签名称
         ax.set_xlabel("Time(UTC, day)", fontsize=14)
@@ -325,42 +426,68 @@ class Draw():
 
         model_list = ['ACM2', 'YSU', 'QNSE', 'QNSE_EDMF', 'TEMF']
 
-        time_index1 = pd.date_range(start='20160702 00', end='20160731 00', freq='24H')
-        time_index2 = pd.date_range(start='20160702 12', end='20160731 12', freq='24H')
+        time_index1 = pd.date_range(start='20160702 00',
+                                    end='20160731 00',
+                                    freq='24H')
+        time_index2 = pd.date_range(start='20160702 12',
+                                    end='20160731 12',
+                                    freq='24H')
         # time_index_dic = {'00':{'data':time_index1, 'name':'00'},
-                            #  '12':{'data':time_index2, 'name':'12'}}
+        #  '12':{'data':time_index2, 'name':'12'}}
 
         # time_index_dic = {'00':time_index1, '12':time_index2}
-        time_index_dic = {'00':time_index1}
+        time_index_dic = {'00': time_index1}
         # time_index_dic = {'12':time_index2}
-        
+
         # for model in model_list:
         for key in time_index_dic:
             time_index = time_index_dic[key]
             CS = None
             for i in range(len(model_list)):
-                title = str(station_name)+model_list[i]+str(key)
-                CS = self.draw_contourf_single(var, axes[i], model_dic[model_list[i]], title, time_index)
+                title = str(station_name) + model_list[i] + str(key)
+                CS = self.draw_contourf_single(var, axes[i],
+                                               model_dic[model_list[i]], title,
+                                               time_index)
 
-            cb = fig.colorbar(CS, orientation='horizontal', shrink=0.8, pad=0.14, fraction=0.14) # 这里的cs是画填色图返回的对象
+            cb = fig.colorbar(CS,
+                              orientation='horizontal',
+                              shrink=0.8,
+                              pad=0.14,
+                              fraction=0.14)  # 这里的cs是画填色图返回的对象
 
             path = '/home/fengxiang/Project/Asses_PBL/Draw/UPAR/'
-            fig_name = os.path.join(path, str(var)+"_"+str(station_name)+"_"+str(key))
+            fig_name = os.path.join(
+                path,
+                str(var) + "_" + str(station_name) + "_" + str(key))
             # fig.savefig('/home/fengxiang/Project/Asses_PBL/Draw/tt.png')
             fig.savefig(fig_name)
-
 
 
 if __name__ == '__main__':
 
     pass
-    station_dic={
-        'GaiZe':{'lat':32.3, 'lon':84.0, 'name':'GaiZe','number':'55248'},
-        'ShenZha':{'lat':30.9, 'lon':88.7, 'name':'ShenZha', 'number':'55472'},
-        'ShiQuanhe':{'lat':32.4, 'lon':80.1, 'name':'ShiQuanhe', 'number':'55228'},
+    station_dic = {
+        'GaiZe': {
+            'lat': 32.3,
+            'lon': 84.0,
+            'name': 'GaiZe',
+            'number': '55248'
+        },
+        'ShenZha': {
+            'lat': 30.9,
+            'lon': 88.7,
+            'name': 'ShenZha',
+            'number': '55472'
+        },
+        'ShiQuanhe': {
+            'lat': 32.4,
+            'lon': 80.1,
+            'name': 'ShiQuanhe',
+            'number': '55228'
+        },
     }
     station = station_dic['GaiZe']
-    # gd = Get_data()
-    # aa = gd.get_data_main('temp', station)
-    Dr = Draw()
-    Dr.draw_main()
+    gd = Get_data()
+    aa = gd.get_data_main('temp_grads', station)
+    # Dr = Draw()
+    # Dr.draw_main()
