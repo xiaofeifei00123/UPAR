4b828330b4fd9a8ee163fc450120a66eeb1db3a7
forxd <2951985709@qq.com>
2021-06-22 09:56:17 +0800

    初步实现所有功能，后面需要修改细节

--- temp.py
+++ temp.py
@@ -270,6 +270,9 @@ class Get_data():
                     ser.append(sr)
             da = xr.concat(ser, 'pressure')
             da.coords['pressure'] = pressure
+
+            da = da.transpose()
+
             dic_return[model] = da
         return dic_return
 
@@ -296,7 +299,7 @@ class Get_data():
             model_dic['obs'] = ds_obs['temp']
 
             model_dic = self.grads_data(model_dic)
-            print(model_dic)
+            # print(model_dic)
             pass
 
         elif var == 't_td_grads':  ## 梯度
@@ -448,6 +451,20 @@ class Draw():
             level = np.arange(0, 15, 1)
         elif var == 'wind':
             level = np.arange(0, 15, 1)
+        elif var == 'temp_grads':
+            level = np.arange(0, 0.5, 0.01)
+        elif var == 't_td_grads':
+            level = np.arange(0, 0.25, 0.01)
+        elif var == 'wind_grads':
+            level = np.arange(0, 0.25, 0.01)
+            
+        # elif var in ['temp_grads', 't_td_grads', 'wind_grads']:
+        #     level = np.arange(0, 0.5, 0.01)
+        #     pass
+        # print(len(x))
+        # print(len(y))
+        # print(val.shape)
+        # print(level)
 
         CS = ax.contourf(x,
                          y,
@@ -464,7 +481,7 @@ class Draw():
 
         plt.gca().invert_yaxis()
         ax.set_ylim(600, 300)
-        ax.set_title(title)
+        ax.set_title(title, fontsize=14)
         # ## 设置标签名称
         ax.set_xlabel("Time(UTC, day)", fontsize=14)
         ax.set_ylabel("Pressure (hPa)", fontsize=14)
@@ -489,7 +506,7 @@ class Draw():
                             bottom=0.18,
                             top=0.95,
                             wspace=0.3,
-                            hspace=0.3)
+                            hspace=0.35)
 
         axes = [None] * 6  # 设置一个维度为8的空列表
         axes[0] = fig.add_subplot(grid[0, 0:1])
@@ -501,53 +518,34 @@ class Draw():
 
         model_list = ['ACM2', 'YSU', 'QNSE', 'QNSE_EDMF', 'TEMF','obs']
 
-        # time_index1 = pd.date_range(start='20160702 00',
-        #                             end='20160731 00',
-        #                             freq='24H')
-        # time_index2 = pd.date_range(start='20160702 12',
-        #                             end='20160731 12',
-        #                             freq='24H')
-        # # time_index_dic = {'00':{'data':time_index1, 'name':'00'},
-        # #  '12':{'data':time_index2, 'name':'12'}}
-
-        # # time_index_dic = {'00':time_index1, '12':time_index2}
-        # time_index_dic = {'00': time_index1}
-        # # time_index_dic = {'12':time_index2}
 
         ax6 = fig.add_axes([0.18, 0.06, 0.7, 0.02])  # 重新生成一个新的坐标图
         keys = ['00', '12']
         # for model in model_list:
-        for i in range(len(model_list)):
-            for key in keys:
-                time_index = model_dic[model_list[i]].time.sel(time=datetime.time(int(key)))
-                # time_index = time_index_dic[key]
-                CS = None
-                # time_index = model_dic[model_list[i]].time.sel(time=datetime.time(12))
-                title = str(station_name) + model_list[i] + str(key)
-                CS = self.draw_contourf_single(var, axes[i],
-                                                model_dic[model_list[i]], title,
-                                                time_index)
+        for key in keys:
+            for i in range(len(model_list)):
+                    time_index = model_dic[model_list[i]].time.sel(time=datetime.time(int(key)))
+                    # time_index = time_index_dic[key]
+                    CS = None
+                    # time_index = model_dic[model_list[i]].time.sel(time=datetime.time(12))
+                    title = str(station_name)+"_"+ model_list[i] +"_"+ str(key)
+                    CS = self.draw_contourf_single(var, axes[i],
+                                                    model_dic[model_list[i]], title,
+                                                    time_index)
             
-            # time_index = model_dic['obs'].time.sel(time=datetime.time(12))
-            # print(time_index)
-            # print(type(time_index))
-            # CS = self.draw_contourf_single(var, axes[5],
-            #                                 model_dic[model_list[5]], title,
-            #                                 time_index)
+            cb = fig.colorbar(CS,
+                                cax=ax6,
+                                orientation='horizontal',
+                                shrink=0.8,
+                                pad=0.14,
+                                fraction=0.14)  # 这里的cs是画填色图返回的对象
 
-        cb = fig.colorbar(CS,
-                            cax=ax6,
-                            orientation='horizontal',
-                            shrink=0.8,
-                            pad=0.14,
-                            fraction=0.14)  # 这里的cs是画填色图返回的对象
-
-        path = '/home/fengxiang/Project/Asses_PBL/Draw/UPAR/picture/'
-        fig_name = os.path.join(
-            path,
-            str(var) + "_" + str(station_name) + "_" + str(key))
-        # fig.savefig('/home/fengxiang/Project/Asses_PBL/Draw/tt.png')
-        fig.savefig(fig_name)
+            path = '/home/fengxiang/Project/Asses_PBL/Draw/UPAR/picture/'
+            fig_name = os.path.join(
+                path,
+                str(var) + "_" + str(station_name) + "_" + str(key))
+            # fig.savefig('/home/fengxiang/Project/Asses_PBL/Draw/tt.png')
+            fig.savefig(fig_name)
 
 
 if __name__ == '__main__':
@@ -577,7 +575,10 @@ if __name__ == '__main__':
     # gd = Get_data()
     # aa = gd.get_data_main('temp', station)
 
-    var = 'temp'
+    # var = 'temp'
+    # var = 't_td'
+    var_list = ['temp', 't_td', 'wind', 'temp_grads', 't_td_grads', 'wind_grads']
+    var = var_list[5]
 
     Dr = Draw()
     Dr.draw_main(station_dic, var)
